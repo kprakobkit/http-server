70df9fba09c87f58a14a1729b1f7e7541969ba66
Peter Prakobkit <kprakobkit@gmail.com>
2015-01-05 16:14:43 -0800

    server able to receive message

--- server.rb
+++ server.rb
@@ -3,10 +3,21 @@ require 'socket'
 server = TCPServer.new 2000
 
 loop do
-  puts "Server running."
-  client = server.accept
+  puts "Server running"
+  socket = server.accept
+  message = socket.gets.chomp
 
-  puts "Response"
-  client.puts "Hello World!"
-  client.close
+  case message
+  when 'home'
+    response = 'Welcome to my Server!'
+  when 'profile'
+    response = [
+      'User: Peter Prakobkit',
+      'Favorite Quote: Chang Ku Yu Nai?'
+    ].join("\n")
+  end
+
+  socket.puts response
+
+  socket.close
 end
